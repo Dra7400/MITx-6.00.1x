bf5990b1557c4d0386eaf52e3737b36eaa6d8645
Dra7400 <88469667+Dra7400@users.noreply.github.com>
2022-09-15 15:26:15 -0700

    Create PSetP3.py

---
+++ Week3/PSetP3.py
@@ -0,0 +1,15 @@
+import string
+alph = string.ascii_lowercase
+
+def getAvailableLetters(lettersGuessed):
+    '''
+    lettersGuessed: list, what letters have been guessed so far
+    returns: string, comprised of letters that represents what letters have not
+      yet been guessed.
+    '''
+    # FILL IN YOUR CODE HERE...
+    remain = []
+    for i in alph:
+        if i not in lettersGuessed:
+            remain.append(i)
+    return ''.join(remain)
