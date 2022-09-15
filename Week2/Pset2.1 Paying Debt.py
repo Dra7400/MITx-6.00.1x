bab17bcf7aeaa553f37046746b42435b3b18acc7
Dra7400 <88469667+Dra7400@users.noreply.github.com>
2022-09-15 15:03:38 -0700

    Create Pset2.1 Paying Debt.py

---
+++ Week2/Pset2.1 Paying Debt.py
@@ -0,0 +1,12 @@
+# Paste your code into this box
+i = 1
+
+while i <= 12:
+    monthlyInterestRate = annualInterestRate/12.0
+    minimumMonthlyPayment = balance * monthlyPaymentRate
+    unpaidBalance = balance - minimumMonthlyPayment
+    updatedBalance = unpaidBalance + monthlyInterestRate * unpaidBalance
+    balance = updatedBalance
+    i += 1
+print("The Remaining Balance is:" + " " + str(round(balance,2)))
+
